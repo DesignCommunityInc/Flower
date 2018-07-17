3b82ff2b260dfda9d9a8d2e2c34dfe437ec53786
DCDAlex <40469691+DCDAlex@users.noreply.github.com>
2018-07-17 16:40:22 +0300

    Add files via upload

--- templates/queries/manager.php
+++ templates/queries/manager.php
@@ -7,6 +7,37 @@
     $get = $_GET;
     $session = $_SESSION;
     $cookie = $_COOKIE;
+    $categories = array();
+
+
+    function header_fill($db){
+        $query = "SELECT `ID`, `category_name` FROM `categories`";
+        $action = mysqli_query($db, $query);
+
+        $categories = array();
+        
+        while ($item = mysqli_fetch_array($action)){
+            $categories[$item["ID"]][] = $item["category_name"];
+        }
+
+        foreach($categories as $it => $s){
+            $subcategories = array();
+            $id = $it;
+            $query1 = "SELECT `ID`, `category_name` FROM `sub_categories` WHERE `parent_id` = '$id'";
+            $action1 = mysqli_query($db, $query1);
+
+            while($item1 = mysqli_fetch_assoc($action1)){
+
+                $subcategories[] = $item1;
+                
+            }
+
+            $categories[$id]["sub"] = $subcategories;     
+        }
+
+        return $categories;
+    }
+   
 
     if($post)
     {
@@ -22,19 +53,15 @@
                 $_news = array();
                 $_sales = array();
                 $_seasons = array();
+                //$_categories = array();
+                //$_subcategories = array();
 
                 $query = "db_query";
 
-                $_news = $query("SELECT `Id`, `name`, `description_min`, `price`, `type`, `image_path` FROM `products` WHERE `type` = 'new'", $db);
-                $_sales = $query("SELECT `Id`, `name`, `description_min`, `price`, `type`, `image_path` FROM `products` WHERE `type` = 'sale'", $db);
-                $_seasons = $query("SELECT `Id`, `name`, `description_min`, `price`, `type`, `image_path` FROM `products` WHERE `type` = 'season'", $db);
-
-                //$query = "SELECT `Id`, `name`, `description_min`, `price`, `type`, `image_path` FROM `products` WHERE `type` = 'new'";
-                //$action = mysqli_query($db, $query);
-
-                //while($item = mysqli_fetch_assoc($action)){
-                //    $_news[] = $item;
-                //}
+                $_news = $query("SELECT `ID`, `title`, `price`, `type`, `image_path` FROM `products` WHERE `type` = 'new'", $db);
+                $_sales = $query("SELECT `ID`, `title`, `price`, `type`, `image_path` FROM `products` WHERE `type` = 'sale'", $db);
+                $_seasons = $query("SELECT `ID`, `title`, `price`, `type`, `image_path` FROM `products` WHERE `type` = 'season'", $db);
+                
 
                 if (count($_news) == 0 || count($_sales) == 0 || count($_seasons) == 0){
                     $errors[] = "Ошибка при поиске товара<br>";
@@ -42,8 +69,10 @@
 
                 if(empty($errors)){
                     $news = $_news;
-                    $sales = $sales;
-                    $seasons = $seasons;
+                    $sales = $_sales;
+                    $seasons = $_seasons;
+                    //$categories = $_categories;
+                    //$subcategories = $_subcategories;
                 }
             } break;
 
